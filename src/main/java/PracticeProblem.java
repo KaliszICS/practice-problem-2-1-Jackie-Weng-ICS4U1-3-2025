4980b03e4d33eca49464f293a25dee1f3b77bb83
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-04 18:47:20 +0000

    COMPLETED!!!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,53 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+ 
+    public static int find(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	}
 
-	
+    public static int findLast(String[] array, String string) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i].equals(string)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
+
+
+    public static int findSecond(char[] array, char character) {
+        int count = 0;
+        int firstIndex = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == character) {
+                count++;
+                if (count == 1) {
+                    firstIndex = i;
+                } else if (count == 2) {
+                    return i;
+                }
+            }
+        }
+
+        return firstIndex != -1 ? firstIndex : -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 2, 7, 2, 5};
+        String[] words = {"doggie", "cat", "poodle", "apple"};
+        char[] letters = {'a', 'b', 'c', 'a', 'd'};
+
+        System.out.println("find: " + find(numbers, 2)); 
+        System.out.println("findLast: " + findLast(words, "apple")); 
+        System.out.println("findSecond: " + findSecond(letters, 'a')); 
+    }
 }
